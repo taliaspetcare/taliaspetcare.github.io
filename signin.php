8b4bd671f4b5163ef469bebb1a2812f32c75fea7
Doodleboy <Doodleboy@users.noreply.github.com>
2014-06-12 15:18:29 -0400

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,9 +1,9 @@
+    <form action="<?php excho $_SERVER['PHP_SELF"]; ?>" method="POST">
+    <input type="text" name="Username">
+    <input type="hidden" name="Password">
+    <input type="submit" name="submit">
+    </form>
     <?php
     $adminusrname = "admin";
     $adminpasswrd = "admin";
-    $echo signinprompt = "<a href="='#'>Sign In</a>"
-    echo signinprompt;
-    if (signinprompt = true){
-      
-    }
     ?>
