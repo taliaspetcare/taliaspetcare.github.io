ebdc8ec9466e22662520811b30b80c4f9cdbb626
Doodleboy <Doodleboy@users.noreply.github.com>
2014-06-12 15:13:37 -0400

    Create signin.php

---
+++ signin.php
@@ -0,0 +1,9 @@
+    <?php
+    $adminusrname = "admin";
+    $adminpasswrd = "admin";
+    $echo signinprompt = "<a href="='#'>Sign In</a>"
+    echo signinprompt;
+    if (signinprompt = true){
+      
+    }
+    ?>
